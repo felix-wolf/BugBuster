902f709cfac250b24dfc1c7160b0bdbc84810e23
Steffen Schubert <8schuber@informatik.uni-hamburg.de>
2019-05-15 16:10:06 +0200

    test

--- Mediathek/Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/VormerkKarteTest.java
+++ Mediathek/Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/VormerkKarteTest.java
@@ -13,7 +13,6 @@ import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.CD;
 import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
 import de.uni_hamburg.informatik.swt.se2.mediathek.services.verleih.VerleihServiceImpl;
 
-<<<<<<< HEAD
 /**
  * Diese Testklasse testet die VormerkKarte
  * @author Gruppe BugBuster
