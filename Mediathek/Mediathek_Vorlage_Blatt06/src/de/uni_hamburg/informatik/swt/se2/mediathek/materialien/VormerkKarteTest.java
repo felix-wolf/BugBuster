fb42b9eff8ca4440a63b54d8954705a7b048e3b9
Steffen Schubert <8schuber@informatik.uni-hamburg.de>
2019-05-15 15:03:55 +0200

    hi

--- Mediathek/Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/VormerkKarteTest.java
+++ Mediathek/Mediathek_Vorlage_Blatt06/src/de/uni_hamburg/informatik/swt/se2/mediathek/materialien/VormerkKarteTest.java
@@ -12,6 +12,11 @@ import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.CD;
 import de.uni_hamburg.informatik.swt.se2.mediathek.materialien.medien.Medium;
 import de.uni_hamburg.informatik.swt.se2.mediathek.services.verleih.VerleihServiceImpl;
 
+/**
+ * Testet die VormerkKarte
+ * @author 8schuber
+ *
+ */
 public class VormerkKarteTest {
 
 	private VormerkKarte _karte;
