5c99adb588bc809f9652c4d26274a3bb3ada6486
Quoc Huy Nguyen <quochuyjava@gmail.com>
2019-06-04 10:54:46 +0200

    Quoc Huys Implementation hinzugefuegt

---
+++ Blatt 07/Kinoticketverkauf_QuocHuy/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/Beobachter.java
@@ -0,0 +1,22 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge;
+
+/**
+ * Interface für Beobachter, die sich für Änderungen eines ObservableSubWerkzeug
+ * interessieren.
+ * 
+ * @author Bugbuster
+ * @version SoSe 2019
+ */
+
+public interface Beobachter
+{
+    /**
+     * Diese Operation wird aufgerufen, sobald sich an an dem beobachteten
+     * Werkzeug etwas relevantes geändert hat.
+     * 
+     * Implementierende Klassen müssen in dieser Operation auf die Aenderung
+     * reagieren.
+     */
+    public void beachteAenderung();
+
+}
