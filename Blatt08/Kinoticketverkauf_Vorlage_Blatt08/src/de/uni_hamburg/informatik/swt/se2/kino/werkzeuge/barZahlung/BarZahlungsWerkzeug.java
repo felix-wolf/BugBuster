8d885c64bfc9fa846579215c530e3cc937189c6c
Felix Wolf <8fwolf@informatik.uni-hamburg.de>
2019-06-26 10:53:43 +0200

    Blatt08

---
+++ Blatt08/Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barZahlung/BarZahlungsWerkzeug.java
@@ -0,0 +1,166 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barZahlung;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Mit diesem Werkzeug wird die Barzahlung für die Tickets abgewickelt.
+ * Dabei wird ein kleines UI-Fenster erzeugt, in dem der zu zahlende
+ * Betrag angezeigt wird und der bezahlte Betrag von Kunden eingetragen werden
+ * kann. Darauf hin wird nach drücken der Enter-Taste das Rückgeld berechnet 
+ * und der OK-Button aktiviert, sollte der Kunde ausreichend Geld bezahlt haben.
+ * 
+ * @author BugBuster-Team
+ * @version SoSe 2019
+ */
+public class BarZahlungsWerkzeug 
+{
+	//ob der ok-Button gedrückt wurde
+	private boolean _okButtonGedrückt = false;
+	//der zu zahlende Preis der Tickets
+	private int _preis;
+	//der eingegebene Betrag
+	private int _betrag;
+	//Die UI
+	private BarZahlungsWerkzeugUI _barZahlungsWerkzeugUI;
+
+	/**
+	 * Initialisiert das BarZahlungsWerkzeug mit einem Preis
+	 * 
+	 * @param preis der zu zahlende Preis der Tickets
+	 * 
+	 * @require preis > 0
+	 * @ensure preis != null
+	 */
+	public BarZahlungsWerkzeug(int preis)
+	{
+		assert preis > 0 : "Vorbedingung verletzt: preis > 0";
+
+		_preis = preis;
+		_barZahlungsWerkzeugUI = new BarZahlungsWerkzeugUI(_preis);
+		registriereUIAktionen();
+		_barZahlungsWerkzeugUI.zeigeFenster();
+
+	}
+
+	/**
+	 * Fügt der UI die Funktionalität hinzu mit entsprechenden Listenern.
+	 * Funktionalität: Beim drücken der Knöpfe wird das Fenster geschlossen,
+	 * Es können nur Zahlen in das Textfield eingegeben werden, während der 
+	 * Nutzer Zahlen einträgt ist der Ok-Button immer disabled, bis Enter
+	 * bei einem gültigen Betrag gedrückt wurde
+	 */
+	private void registriereUIAktionen() 
+	{
+		_barZahlungsWerkzeugUI.getOkButton().addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				_okButtonGedrückt = true;
+				_barZahlungsWerkzeugUI.schliesseFenster();
+			}
+		});
+
+		_barZahlungsWerkzeugUI.getAbbrechenButton().addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				_barZahlungsWerkzeugUI.schliesseFenster();
+			}
+		});
+
+		_barZahlungsWerkzeugUI.getBezahltTextfield().addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (!Character.isDigit(e.getKeyChar()))
+				{
+					e.consume();            				
+				}	
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() != 10)
+				{
+					_barZahlungsWerkzeugUI.aktiviereOKButton(false);
+				}
+				else if (istBezahlenMoeglich())
+				{	
+					_okButtonGedrückt = true;
+					_barZahlungsWerkzeugUI.schliesseFenster();
+				}
+				else 
+				{
+						reagiereAufEinzahlung();
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+			}
+
+		});
+	}
+
+	/**
+	 * Verarbeitet die Einzahlung, indem der OK-Button abhängig davon,
+	 * ob genug Geld bezahlt wurde, freigeschalten/ausgeschalten wird
+	 * und die Restbetragsanzeige aktualisiert wird
+	 */
+	private void reagiereAufEinzahlung()
+	{
+		_betrag = (int) _barZahlungsWerkzeugUI.getBezahlt();
+		_barZahlungsWerkzeugUI.aktiviereOKButton(istBezahlenMoeglich());		
+		aktualisiereRestbetragAnzeige(_betrag);
+	}
+
+	/**
+	 * gibt zurück, ob der Restbetrag >= 0 ist, also genug Geld bezahlt wurde
+	 * 
+	 * @return ob Restbetrag >= 0 
+	 */
+	private boolean istBezahlenMoeglich() 
+	{
+		return berechneRestbetrag(_betrag) >= 0; 
+	}
+
+	/**
+	 * aktualisiert die Restbetragsanzeige der UI
+	 * 
+	 * @param betrag der neue Restbetrag
+	 */
+	private void aktualisiereRestbetragAnzeige(int betrag) 
+	{
+		_barZahlungsWerkzeugUI.setzeRestlabel(berechneRestbetrag(betrag));
+	}
+
+	/**
+	 * berechnet den neuen Restbetrag
+	 * 
+	 * @require betrag >= 0
+	 * @param betrag der eingezahlte Betrag
+	 * @return der neue Restbetrag
+	 */
+	private int berechneRestbetrag(int betrag) 
+	{
+		assert betrag >= 0 : "Vorbedingung verletzt: betrag >= 0";
+		
+		return betrag - _preis;
+	}
+
+	/**
+	 * Gibt zurück, ob der OKButton in der UI gedrückt wurde,
+	 * das Geld also bezahlt wurde
+	 * 
+	 * @return ob bezahlt wurde
+	 */
+	public boolean istBezahlt()
+	{
+		return _okButtonGedrückt;
+	}
+}
---
+++ Blatt08/Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barZahlung/BarZahlungsWerkzeugUI.java
@@ -0,0 +1,150 @@
+package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barZahlung;
+
+import java.awt.GridLayout;
+import java.text.DecimalFormat;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+
+public class BarZahlungsWerkzeugUI {
+
+	//UI-Elemente
+	private JDialog _dialog;
+	private JButton _okButton;
+	private JButton _abbrechenButton;
+	private JLabel _betragTextLabel;
+	private JLabel _betragLabel;
+	private JLabel _restTextLabel;
+	private JLabel _restLabel;
+	private JLabel _bezahltLabel;
+	private JFormattedTextField _bezahltTextfield ;
+
+	/**
+	 * Initialisiert die BarZahlungsoberfläche
+	 * 
+	 * @param betrag der zu zahlende Betrag
+	 */
+	public BarZahlungsWerkzeugUI(int betrag) {
+		_dialog = new JDialog(_dialog, true);
+		_dialog.setTitle("Barzahlung");
+		_dialog.setLayout(new GridLayout(4, 2, 20, 20));
+		_dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+		createFields(betrag);
+		createButtons();		
+		_dialog.pack();
+	}
+
+	/**
+	 * Gibt den OKButton zurück
+	 * 
+	 * @return OKButton
+	 */
+	public JButton getOkButton() {
+		return _okButton;
+	}
+
+	/**
+	 * Gibt den AbbrechenButton zurück
+	 * 
+	 * @return AbbrechenButton
+	 */
+	public JButton getAbbrechenButton() {
+		return _abbrechenButton;
+	}
+
+	/**
+	 * Gibt das Textfeld zurück
+	 * 
+	 * @return _bezahltTextfield
+	 */
+	public JFormattedTextField getBezahltTextfield() {
+		return _bezahltTextfield;
+	}
+
+	/**
+	 * Gibt das RestLabel zurück
+	 * 
+	 * @return _restLabel
+	 */
+	public JLabel getRestLabel() {
+		return _restLabel;
+	}
+
+	public void setzeRestlabel(int betrag) {
+		_restLabel.setText(betrag + " Eurocent");
+	}
+
+	/**
+	 * Setzt den OKButton auf true/false
+	 */
+	public void aktiviereOKButton(Boolean bool) {
+		_okButton.setEnabled(bool);
+	}
+
+	/**
+	 * zeigt die BarZahlungsoberfläche an
+	 */
+	public void zeigeFenster() {
+		_dialog.setVisible(true);
+	}
+
+	/**
+	 * schließt die BarZahlungsoberfläche
+	 */
+	public void schliesseFenster() {
+		_dialog.dispose();
+	}
+
+	/**
+	 * Erzeugt alle Label und setzt die Texte.
+	 * Fügt die Labels zum Dialog hinzu
+	 * 
+	 * @param betrag der zu zahlende Betrag
+	 */
+	private void createFields(int betrag) {
+		_betragTextLabel = new	JLabel("Gesamtbetrag:");
+		_betragLabel = new JLabel();
+
+		_restTextLabel = new JLabel("Rückgeld:");
+		_restLabel = new JLabel();
+		_restLabel.setText("0");
+
+		_bezahltLabel = new JLabel("Bezahlt:");
+		_bezahltTextfield = new JFormattedTextField(new DecimalFormat());
+
+		_dialog.add(_betragTextLabel);
+		_dialog.add(_betragLabel);
+		_dialog.add(_bezahltLabel);
+		_dialog.add(_bezahltTextfield);
+		_dialog.add(_restTextLabel);
+		_dialog.add(_restLabel);
+		_betragLabel.setText(String.valueOf(betrag));
+	}
+
+	/**
+	 * erzeugt die Buttons und setzt die Texte
+	 * Fügt die Buttons zum Dialog hinzu
+	 */
+	private void createButtons() {
+		_okButton = new JButton("OK");
+		_okButton.setEnabled(false);
+		_abbrechenButton = new JButton("Abbrechen");
+		_dialog.add(_abbrechenButton);
+		_dialog.add(_okButton);
+
+	}
+
+	/**
+	 * Gibt den Inhalt des Textfields zurück
+	 * Wenn es null ist, gibt 0 zurück
+	 * 
+	 * @return Inhalt des Textfields oder 0
+	 * 
+	 */
+	public long getBezahlt() {
+		return _bezahltTextfield.getValue() != null ? (long) _bezahltTextfield.getValue() : 0;
+
+	}
+}
