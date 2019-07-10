4b7ed54b427250e079062599b7daec62bb1e9f8d
Felix Wolf <8fwolf@informatik.uni-hamburg.de>
2019-07-10 09:32:39 +0200

    Kommentare

--- Blatt 09/Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ Blatt 09/Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -9,7 +9,7 @@ public class Geldbetrag {
 	private static HashMap<Integer, Geldbetrag> GELDBETRAEGE = new HashMap<Integer, Geldbetrag>();
 	//interne Darstellung eines Geldbetrags
 	private final int _eurocent;
-	
+
 	/**
 	 * Privater Konstruktor, fügt "sich selbst" in die HashMap ein
 	 * 
@@ -20,44 +20,53 @@ public class Geldbetrag {
 	private Geldbetrag(int eurocent) throws Exception {
 		assert istGueltig(eurocent): "Vorbedingung verletzt: istGueltig(eurocent)";
 		_eurocent = eurocent;
-		
+
 		GELDBETRAEGE.put(eurocent, this);
 	}
-	
+
 	/**
+	 * Liefert Geldbetrag aus einem long
 	 * 
-	 * @param eurocent
-	 * @return
-	 * @throws Exception
+	 * @param eurocent die Höhe des Geldbetrags
+	 * @return der Geldbetrag
+	 * @require istGueltig(eurocent)
+	 * @throws Exception wenn eurocent ungültig ist (außerhalb Wertebereich int)
 	 */
 	public static Geldbetrag get(long eurocent) throws Exception
 	{
-	    
+
+		assert istGueltig(eurocent) : "Vorbedingung verletzt: istGueltig(eurocent)";
+
 		try
 		{
 			istGueltig(eurocent);
 		}
 		catch(Exception e)
 		{	
-		    throw new Exception(e.getMessage());
+			throw new Exception(e.getMessage());
 		}
-		
 		int betrag = (int) eurocent;
-		if(!GELDBETRAEGE.containsKey(betrag)) {
-			return new Geldbetrag(betrag);
-		}
-		else
-		{
-		 return GELDBETRAEGE.get(betrag);	
-		}
+		
+		return (!GELDBETRAEGE.containsKey(betrag) ? new Geldbetrag(betrag) : GELDBETRAEGE.get(betrag));
+
 	}
-	
+
+	/**
+	 * Liefert Geldbetrag aus einem String
+	 * 
+	 * @param euroString die Höhe des Geldbetrags
+	 * @return der Geldbetrag
+	 * @require euroString != null
+	 * @throws Exception wenn euroString ungültig ist
+	 */
 	public static Geldbetrag get(String euroString) {
+		
+		assert euroString != null : "Vorbedingung verletzt: euroString != null";
 		
 		try
 		{
 			istGueltig(euroString);
-		    return Geldbetrag.get(stringToEuroCent(euroString));
+			return Geldbetrag.get(stringToEuroCent(euroString));
 
 		}
 		catch(Exception e)
@@ -66,13 +75,22 @@ public class Geldbetrag {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * addiert einen Geldbetrag zu einem anderen
+	 * 
+	 * @param geldbetrag der zu addierende Geldbetrag
+	 * @return den neuen Geldbetrag
+	 * @require geldbetrag != null
+	 */
 	public Geldbetrag addiere(Geldbetrag geldbetrag) {
+
+		assert geldbetrag != null : "Vorbedingung verletzt: geldbetrag != null";
 		
 		long betrag1 = this.getEurocent();
 		long betrag2 = geldbetrag.getEurocent();
 		long ergebnis = betrag1 + betrag2;
-		
+
 		try
 		{
 			istGueltig(ergebnis);
@@ -85,12 +103,22 @@ public class Geldbetrag {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Subtrahiert einen Geldbetrag von einem anderen
+	 * 
+	 * @param geldbetrag der Geldbetrag, der abgezogen werden soll
+	 * @return der neue Geldbetrag
+	 * @require geldbetrag != null
+	 */
 	public Geldbetrag subtrahiere(Geldbetrag geldbetrag) {
+		
+		assert geldbetrag != null : "Vorbedingung verletzt: geldbetrag != null";
+		
 		long betrag1 = getEurocent();
 		long betrag2 = geldbetrag.getEurocent(); 
 		long ergebnis = betrag1 - betrag2;
-		
+
 		try
 		{
 			istGueltig(ergebnis);
@@ -103,7 +131,7 @@ public class Geldbetrag {
 		}
 		return null;
 	}
-	
+
 	public Geldbetrag multipliziere(int zahl) {
 		long betrag1 = getEurocent(); 
 		long ergebnis = betrag1 * zahl;
@@ -119,13 +147,13 @@ public class Geldbetrag {
 		}
 		return null;	
 	}
-	
-	
+
+
 	private int getEurocent()
 	{
 		return _eurocent;
 	}
-	
+
 	/**
 	 * 
 	 * @param EuroString
@@ -136,13 +164,13 @@ public class Geldbetrag {
 		if (euroString.contains(","))
 		{
 			return Long.valueOf(euroString.replaceAll(",", "").toString());
-	
+
 		}
 		return Long.valueOf(euroString) * 100;
 
 	}
-	
-	
+
+
 	/**
 	 * Prüfen, ob die Eingage in Interger gültig für Geldbetrag ist.
 	 * 
@@ -159,105 +187,105 @@ public class Geldbetrag {
 		}
 		return true;
 	}
-	
-	   /**
-     * Prüfen, ob die Eingage in String gültig für Geldbetrag ist.
-     * 
-     * @param geldbetragInString : ein String beschreibt Geldbetrag z.B: -12,43
-     * 
-     * @return true, wenn es gültig ist
-     */
+
+	/**
+	 * Prüfen, ob die Eingage in String gültig für Geldbetrag ist.
+	 * 
+	 * @param geldbetragInString : ein String beschreibt Geldbetrag z.B: -12,43
+	 * 
+	 * @return true, wenn es gültig ist
+	 */
 	public static void istGueltig(String geldbetragInString) throws Exception
 	{
-	    if (!gueltigesStringformat(geldbetragInString) || !gueltigeKommaAnzahl(geldbetragInString)
-	    		|| !gueltigeNachkommastellen(geldbetragInString))
-	    {
-	        throw new Exception("Fehler: String nicht im richtigen Format");
-	    }
-	    	
+		if (!gueltigesStringformat(geldbetragInString) || !gueltigeKommaAnzahl(geldbetragInString)
+				|| !gueltigeNachkommastellen(geldbetragInString))
+		{
+			throw new Exception("Fehler: String nicht im richtigen Format");
+		}
+
 	}
-	
+
 	/**
 	 * Prüfe, ob das eingegebene String in Format:  wenn es '-' gibt, dann muss '-' am Anfang steht. Die nächsten Symbole sind aus Nummer oder ',' oder '.'
 	 * @return true, wenn es den Format hat.
 	 */
 	private static boolean gueltigesStringformat (String geldbetragInString)
 	{
-	    char ersteBuchstabe = geldbetragInString.charAt(0);
-        int anfangPositionZumParse;
-        
-        //Wenn es ein Vorzeichen - gibt, dann fängt in der Stelle 1 zu parsen
-        if (ersteBuchstabe == '-') 
-        {
-            anfangPositionZumParse = 1;
-        }
-        else anfangPositionZumParse = 0;
-        
-        for (int i=anfangPositionZumParse; i<geldbetragInString.length(); i++)
-        {
-            char charInString = geldbetragInString.charAt(i);
-            boolean istCharEinNummer = Character.isDigit(charInString);
-            
-            if ( !istCharEinNummer && charInString != ',') 
-            {
-                return false;
-            }
-        }
-        return true;
-	    
-	}
-	
-	   /**
-     * Prüfe, ob das eingegebene String maximal ein ',' Symbol hat
-     * @return true, wenn das String maximal ein ',' oder Symbol hat
-     */
-    private static boolean gueltigeKommaAnzahl (String geldbetragInString)
-    {
-        int AnzahlVorkommen = 0;
-        
-        for (int i=0; i<geldbetragInString.length(); i++)
-        {
-            char charInString = geldbetragInString.charAt(i);
-            
-            if (charInString == ',') 
-            {
-                AnzahlVorkommen++;
-            }
-        }
-        return (AnzahlVorkommen < 2); 
-        
-    }
-    
-    private static boolean gueltigeNachkommastellen (String geldbetragInString)
-    {
-        int count = 0;
-        if (geldbetragInString.contains(","))
-        {
-            int indexVonKomma = geldbetragInString.indexOf(',');
-            for (int i = indexVonKomma; i < geldbetragInString.length() ;i++)
-            {
-                count++;
-            }
-        }
-        return count < 4;
-    }
-    
+		char ersteBuchstabe = geldbetragInString.charAt(0);
+		int anfangPositionZumParse;
 
-    public boolean istNegativ()
-    {
-    	return _eurocent < 0;
-    }
-    
+		//Wenn es ein Vorzeichen - gibt, dann fängt in der Stelle 1 zu parsen
+		if (ersteBuchstabe == '-') 
+		{
+			anfangPositionZumParse = 1;
+		}
+		else anfangPositionZumParse = 0;
+
+		for (int i=anfangPositionZumParse; i<geldbetragInString.length(); i++)
+		{
+			char charInString = geldbetragInString.charAt(i);
+			boolean istCharEinNummer = Character.isDigit(charInString);
+
+			if ( !istCharEinNummer && charInString != ',') 
+			{
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+	/**
+	 * Prüfe, ob das eingegebene String maximal ein ',' Symbol hat
+	 * @return true, wenn das String maximal ein ',' oder Symbol hat
+	 */
+	private static boolean gueltigeKommaAnzahl (String geldbetragInString)
+	{
+		int AnzahlVorkommen = 0;
+
+		for (int i=0; i<geldbetragInString.length(); i++)
+		{
+			char charInString = geldbetragInString.charAt(i);
+
+			if (charInString == ',') 
+			{
+				AnzahlVorkommen++;
+			}
+		}
+		return (AnzahlVorkommen < 2); 
+
+	}
+
+	private static boolean gueltigeNachkommastellen (String geldbetragInString)
+	{
+		int count = 0;
+		if (geldbetragInString.contains(","))
+		{
+			int indexVonKomma = geldbetragInString.indexOf(',');
+			for (int i = indexVonKomma; i < geldbetragInString.length() ;i++)
+			{
+				count++;
+			}
+		}
+		return count < 4;
+	}
+
+
+	public boolean istNegativ()
+	{
+		return _eurocent < 0;
+	}
+
 	@Override
 	public String toString() {
-		
+
 		int euro = Math.abs(_eurocent / 100);
 		int cent = Math.abs(_eurocent % 100);
 		String centString = "" + cent;
 		if (cent < 10) {
 			centString = "0" + centString;
 		}
-		
+
 		String minus = "";
 		if (istNegativ())
 		{
@@ -265,22 +293,22 @@ public class Geldbetrag {
 		}
 		return minus + euro + "," + centString;
 	}
-	
-    @Override
-    public boolean equals(Object o)
-    {
-        return (o instanceof Geldbetrag) && equals((Geldbetrag) o);
-    }
 
-    private boolean equals(Geldbetrag andererGeldbetrag)
-    {
-        return (_eurocent == andererGeldbetrag._eurocent);
-    }
+	@Override
+	public boolean equals(Object o)
+	{
+		return (o instanceof Geldbetrag) && equals((Geldbetrag) o);
+	}
 
-    @Override
-    public int hashCode()
-    {
-        return 10004 * _eurocent;
-    }
+	private boolean equals(Geldbetrag andererGeldbetrag)
+	{
+		return (_eurocent == andererGeldbetrag._eurocent);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return 10004 * _eurocent;
+	}
 
 }
